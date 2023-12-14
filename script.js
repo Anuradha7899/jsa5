c18ce3d7631707f2a0e107df2114dc8a840239cb
Anuradha7899 <142803316+Anuradha7899@users.noreply.github.com>
2023-12-14 17:26:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// All data types javascript...
+//Number
+let a = 45;
+let b = 45.98;
+
+//srting....
+let str = "My string ";
+let str2 = "My string code in javascript";
+
+
+//undefind....
+let x;
+let y;
+
+//Null....
+let car = null;
+let bike = null;
+console.log(car);
+
+//boolean...
+let a1= 3;
+let b1 = 4;
+let r1 = (a1>b1);
+let r2 = (a1<b1);
+console.log(r1);
+console.log(r2);
